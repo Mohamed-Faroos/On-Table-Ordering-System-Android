c4b81f78ecbd8ada26f96b9af5b1a3e237d093fe
Mohamed-Faroos <faroos1996@gmail.com>
2020-02-26 23:30:30 +0530

    Implemented Tablet Login with backend

---
+++ app/src/main/java/com/otos/app/actions/Login.java
@@ -0,0 +1,111 @@
+package com.otos.app.actions;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.otos.app.LoginActivity;
+import com.otos.app.MenuActivity;
+import com.otos.app.mainFiles.User;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Login extends AsyncTask<String,Void,String> {
+
+    Context context;
+    String id;
+    String password;
+    AlertDialog alertDialog;
+    String log_url="http://192.168.8.101/OTOS/login.php";
+
+    public Login(LoginActivity loginActivity, User us)
+    {
+        User user=us;
+        id=user.getId();
+        password=user.getPassword();
+        context=loginActivity;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String result="";
+        String req=strings[0];
+
+            try {
+                URL url = new URL(log_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("t_id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8") + "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+
+
+                bufferedReader.close();
+                httpURLConnection.disconnect();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        return result;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+
+        alertDialog=new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Failed");
+    }
+
+
+    @Override
+    protected void onPostExecute(String res) {
+        super.onPostExecute(res);
+
+        if(res.equals("success"))
+        {
+            Intent in = new Intent(context, MenuActivity.class);
+            in.putExtra("table", id);
+            context.startActivity(in);
+            Toast toast = Toast.makeText(context, "Login Successfull", Toast.LENGTH_LONG);
+            toast.show();
+
+        }else{
+
+            alertDialog.setMessage("Please Check Login Details");
+            alertDialog.show();
+        }
+
+
+    }
+}
